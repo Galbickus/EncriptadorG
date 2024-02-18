412aa8197c1b99629d2a9f5a3b58b176f5b22eb7
Veronica <vgalbicka@gmail.com>
2024-02-18 13:13:02 -0300

    encrypt function

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const encryptText = document.querySelector(".user-text");
 const copyText = document.querySelector(".inputCopy");
 let message
+
+
+function encrypt() {
+    if (encryptText.value) {
+      message = encryptText.value
+        .replaceAll("e", "enter")
+        .replaceAll("i", "imes")
+        .replaceAll("a", "ai")
+        .replaceAll("u", "ufat")
+        .replaceAll("o", "ober");
+  
+      copyText.value = message;
+      encryptText.value = "";
+      textFound.classList.remove("display-none");
+      notFound.classList.add("display-none");
+    } else {
+      textFound.classList.add("display-none");
+      notFound.classList.remove("display-none");
+    }
+  }
